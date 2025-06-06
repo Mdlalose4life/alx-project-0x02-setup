62851cabdd9115630c5d6c13763e655c197add7f
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-06-06 10:07:26 +0200

    The props

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,4 +1,6 @@
-const Card: React.FC <CardProps> = ({title, content}) => {
+import { CardProps } from "@/interfaces"
+
+const Card: React.FC<CardProps> = ({title, content}) => {
     return (
         <div className="flex flex-row items-center">
             <p>{title}</p>
